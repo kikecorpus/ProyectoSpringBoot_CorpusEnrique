8bc192ccad9a80905b8b30a84fe93b1ac65c7c05
Enrique <enrique.corpus.5004@hotmail.com>
2026-03-11 08:55:23 -0500

    Feat: :sparkles: Add Models entity-relation and SQL Script

---
+++ src/main/java/com/Campusland/ProyectoSpringBoot_CorpusEnrique/model/Bodega.java
@@ -0,0 +1,11 @@
+package com.Campusland.ProyectoSpringBoot_CorpusEnrique.model;
+
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Table(name = "bodega")
+@Data
+public class Bodega {
+
+
+}
